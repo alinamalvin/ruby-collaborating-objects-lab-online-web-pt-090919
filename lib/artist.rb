ceb6de093b28229d9f9279e5742a7feb98618857
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-14 21:47:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,37 @@
-require "pry"
-class Artist 
-
+class Artist
   attr_accessor :name
 
   @@all = []
 
-  def initialize(name = @name)
+  def initialize(name)
     @name = name
-    @songs = []
-    @@all << self
-    #binding.pry
+    @songs= []
   end
 
-  def songs 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    song
+  end
+
+  def songs
     @songs
   end
 
-  def add_song(song = @song)
-    @songs << song
-  end
-
-  def self.find_or_create_by_name(name = @name)
-    #binding.pry
-    if (self.name.nil?)
-      self.new.name = @name
-      Artist.name
-    else
-      Artist.name
-    end
+  def save
+    @@all << self
+    self
   end
 
   def self.all
     @@all
   end
-end 
+
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name} 
+   end
+end
